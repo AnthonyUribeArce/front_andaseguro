6e04531f70e576a81bb7b1bbf51da499303dd1f3
AnthonyUribeArce <81380536+AnthonyUribeArce@users.noreply.github.com>
2022-11-11 17:29:02 -0500

    despliegue

---
+++ src/app/model/policestation.ts
@@ -0,0 +1,8 @@
+import { Direction } from './direction';
+
+export class PoliceStation {
+    idPolicestation: number = 0;
+    direction:Direction = new Direction();
+    npolicestation: string = "";
+    tcell: string = "";
+}
